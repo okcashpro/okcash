797a9a6bb4b36e3da8be51556add78c383cb483d
Warren Togami <wtogami@gmail.com>
2013-10-25 23:21:21 -1000

    Split MIN_PROTO_VERSION into INIT_PROTO_VERSION and MIN_PEER_PROTO_VERSION
    INIT_PROTO_VERSION is the initial version, after a succesful version/verack it is increased to a negotiated version.
    MIN_PEER_PROTO_VERSION could be a different value to disconnect from peers older than a specified version.
    
    Conflicts:
    	src/main.cpp
    	src/net.h

--- src/version.h
+++ src/version.h
@@ -32,8 +32,11 @@ static const int DATABASE_VERSION = 70509;
 
 static const int PROTOCOL_VERSION = 60013;
 
-// earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 209;
+// intial proto version, to be increased after version/verack negotiation
+static const int INIT_PROTO_VERSION = 209;
+
+// disconnect from peers older than this proto version
+static const int MIN_PEER_PROTO_VERSION = 209;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
