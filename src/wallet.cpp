feae605b9ce5463ff28a3df85d2eb43fd6df8f24
Wladimir J. van der Laan <laanwj@gmail.com>
2013-12-19 09:58:06 +0100

    LoadWallet: acquire cs_wallet mutex before clearing setKeyPool
    Make the function mutex-aware, to prevent having to lock cs_wallet
    at the call site in Init.

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1855,7 +1855,6 @@ string CWallet::SendMoneyToDestination(const CTxDestination& address, int64_t nV
 
 DBErrors CWallet::LoadWallet(bool& fFirstRunRet)
 {
-    AssertLockHeld(cs_wallet); // setKeyPool
     if (!fFileBacked)
         return DB_LOAD_OK;
     fFirstRunRet = false;
@@ -1864,6 +1863,7 @@ DBErrors CWallet::LoadWallet(bool& fFirstRunRet)
     {
         if (CDB::Rewrite(strWalletFile, "\x04pool"))
         {
+            LOCK(cs_wallet);
             setKeyPool.clear();
             // Note: can't top-up keypool here, because wallet is locked.
             // User will be prompted to unlock wallet the next operation
