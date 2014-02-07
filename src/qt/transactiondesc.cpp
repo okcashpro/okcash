c6cdbc383e9551869cf041eaf88e31be1bc150ba
Pavel Vasin <rat4vier@gmail.com>
2014-02-07 16:34:49 +0400

    bump number of confirmations to 10

--- src/qt/transactiondesc.cpp
+++ src/qt/transactiondesc.cpp
@@ -23,7 +23,7 @@ QString TransactionDesc::FormatTxStatus(const CWalletTx& wtx)
         int nDepth = wtx.GetDepthInMainChain();
         if (GetAdjustedTime() - wtx.nTimeReceived > 2 * 60 && wtx.GetRequestCount() == 0)
             return tr("%1/offline").arg(nDepth);
-        else if (nDepth < 6)
+        else if (nDepth < 10)
             return tr("%1/unconfirmed").arg(nDepth);
         else
             return tr("%1 confirmations").arg(nDepth);
