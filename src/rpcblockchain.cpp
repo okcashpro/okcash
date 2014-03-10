c40e78fece65351b4d641d59c4d41334228f20f4
Pavel Vasin <rat4vier@gmail.com>
2014-03-10 12:56:52 +0400

    fix netmhashps

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -45,6 +45,9 @@ double GetDifficulty(const CBlockIndex* blockindex)
 
 double GetPoWMHashPS()
 {
+    if (pindexBest->nHeight >= LAST_POW_BLOCK)
+        return 0;
+
     int nPoWInterval = 72;
     int64 nTargetSpacingWorkMin = 30, nTargetSpacingWork = 30;
 
