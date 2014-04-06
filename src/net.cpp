87487df42ab292106d759e405ae1c63b68869b58
Pavel Vasin <rat4vier@gmail.com>
2014-04-06 17:49:47 +0400

    add dns seed

--- src/net.cpp
+++ src/net.cpp
@@ -1143,6 +1143,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
+    {"rat4.blackcoin.co", "seed.blackcoin.co"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
