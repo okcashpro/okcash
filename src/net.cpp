6225d1c1119e8cb446815992a96ce32e15805ede
Gregory Maxwell <greg@xiph.org>
2014-04-09 09:25:52 -0700

    Prevent socket leak in ThreadSocketHandler.
    When we are over our outbound limit ThreadSocketHandler would try to
     keep the connection if the peer was addnoded.
    
    This didn't actually work for two reasons: It didn't actually run
     the accept code due to mistaken code flow, and because we have a
     limited number of outbound semaphores it couldn't actually use the
     connection.
    
    Instead it leaked the socket, which might have caused issue #4034.
    
    This patch just takes out the non-functioning white-listing for now.

--- src/net.cpp
+++ src/net.cpp
@@ -824,11 +824,7 @@ void ThreadSocketHandler2(void* parg)
             }
             else if (nInbound >= GetArg("-maxconnections", 125) - MAX_OUTBOUND_CONNECTIONS)
             {
-                {
-                    LOCK(cs_setservAddNodeAddresses);
-                    if (!setservAddNodeAddresses.count(addr))
-                        closesocket(hSocket);
-                }
+                closesocket(hSocket);
             }
             else if (CNode::IsBanned(addr))
             {
