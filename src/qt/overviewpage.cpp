c855512156a3d6fdcfd8a521b616269068902a94
Pavel Vasin <rat4vier@gmail.com>
2014-04-15 15:55:17 +0400

    gui: add total balance

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -139,6 +139,7 @@ void OverviewPage::setBalance(qint64 balance, qint64 stake, qint64 unconfirmedBa
     ui->labelStake->setText(BitcoinUnits::formatWithUnit(unit, stake));
     ui->labelUnconfirmed->setText(BitcoinUnits::formatWithUnit(unit, unconfirmedBalance));
     ui->labelImmature->setText(BitcoinUnits::formatWithUnit(unit, immatureBalance));
+    ui->labelTotal->setText(BitcoinUnits::formatWithUnit(unit, balance + stake + unconfirmedBalance + immatureBalance));
 
     // only show immature (newly mined) balance if it's non-zero, so as not to complicate things
     // for the non-mining users
