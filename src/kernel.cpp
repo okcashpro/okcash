83a81fdaef952405f6799985019b2dccffb33020
Pavel Vasin <rat4vier@gmail.com>
2014-04-28 20:51:33 +0400

    add stake mod checkpoint

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -19,6 +19,7 @@ static std::map<int, unsigned int> mapStakeModifierCheckpoints =
     boost::assign::map_list_of
         ( 0, 0x0e00670bu )
         ( 1600, 0x1b3404a2 )
+        ( 7000, 0x4da1176e )
     ;
 
 // Hard checkpoints of stake modifiers to ensure they are deterministic (testNet)
