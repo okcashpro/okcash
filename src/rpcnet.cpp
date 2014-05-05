7f81a35a9d4ebdea3cbaaa73c07aa88bc4104190
Kamil Domanski <kdomanski@kdemail.net>
2014-05-05 20:08:13 +0200

    switch from boost int types to <stdint.h>
    Conflicts:
    	src/rpcblockchain.cpp
    	src/rpcclient.cpp
    	src/rpcmining.cpp
    	src/rpcmisc.cpp
    	src/rpcnet.cpp
    	src/rpcrawtransaction.cpp
    	src/rpcwallet.cpp

--- src/rpcnet.cpp
+++ src/rpcnet.cpp
@@ -53,9 +53,9 @@ Value getpeerinfo(const Array& params, bool fHelp)
 
         obj.push_back(Pair("addr", stats.addrName));
         obj.push_back(Pair("services", strprintf("%08"PRIx64, stats.nServices)));
-        obj.push_back(Pair("lastsend", (boost::int64_t)stats.nLastSend));
-        obj.push_back(Pair("lastrecv", (boost::int64_t)stats.nLastRecv));
-        obj.push_back(Pair("conntime", (boost::int64_t)stats.nTimeConnected));
+        obj.push_back(Pair("lastsend", (int64_t)stats.nLastSend));
+        obj.push_back(Pair("lastrecv", (int64_t)stats.nLastRecv));
+        obj.push_back(Pair("conntime", (int64_t)stats.nTimeConnected));
         obj.push_back(Pair("version", stats.nVersion));
         obj.push_back(Pair("subver", stats.strSubVer));
         obj.push_back(Pair("inbound", stats.fInbound));
