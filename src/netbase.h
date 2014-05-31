b911ef11b4ca3b9792644ebe4a41b94a85df3597
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-05-31 12:04:34 +0200

    remove dup of extern int nConnectTimeout; in netbase

--- src/netbase.h
+++ src/netbase.h
@@ -11,6 +11,7 @@
 #include "compat.h"
 
 extern int nConnectTimeout;
+extern bool fNameLookup;
 
 #ifdef WIN32
 // In MSVC, this is defined as a macro, undefine it to prevent a compile and link error
@@ -27,9 +28,6 @@ enum Network
 
     NET_MAX,
 };
-
-extern int nConnectTimeout;
-extern bool fNameLookup;
 
 /** IP address (IPv6, or IPv4 using mapped IPv6 range (::FFFF:0:0/96)) */
 class CNetAddr
