9ceb6f54b72454be840eb19ae15b97a39d158d38
Patrick Doetsch <creon@ativ>
2014-06-24 01:03:49 -0700

    fix relock on sending coins when "for staking only" is activated
    Sets the checkbox in the askpassphrase dialog to the previous value of
    fWalletUnlockStakingOnly resolving the issue that the wallet gets locked
    after sending coins when "for staking only" is activated.

--- src/qt/askpassphrasedialog.cpp
+++ src/qt/askpassphrasedialog.cpp
@@ -27,6 +27,8 @@ AskPassphraseDialog::AskPassphraseDialog(Mode mode, QWidget *parent) :
     ui->passEdit2->installEventFilter(this);
     ui->passEdit3->installEventFilter(this);
 
+    ui->stakingCheckBox->setChecked(fWalletUnlockStakingOnly);
+
     switch(mode)
     {
         case Encrypt: // Ask passphrase x2
