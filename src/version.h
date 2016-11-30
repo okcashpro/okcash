44509fcddc44cad5c618eb32918c9421dd2198d9
oktoshi <devteam@okcash.co>
2016-11-30 02:52:47 -0600

    OK v3.0.0.3

--- src/version.h
+++ src/version.h
@@ -30,20 +30,20 @@ static const int DATABASE_VERSION = 70510;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60018;
+static const int PROTOCOL_VERSION = 60019;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60016;
+static const int MIN_PEER_PROTO_VERSION = 60018;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
 
-static const int MIN_THIN_VERSION = 60016;
-static const int MIN_MBLK_VERSION = 60016;
+static const int MIN_THIN_VERSION = 60018;
+static const int MIN_MBLK_VERSION = 60018;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
