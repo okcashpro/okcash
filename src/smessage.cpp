9e7f7c73ceb9de3e9084d01175556ae1443b8c00
oktoshi <devteam@okcash.co>
2017-09-29 12:46:37 -0500

    typo

--- src/smessage.cpp
+++ src/smessage.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2014 The OKCash developers
+// Copyright (c) 2014 The Okcash Developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
