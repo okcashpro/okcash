195ecb0c6c02baa35d27ff03431ddfc421c4b782
oktoshi <9932412+oktoshi@users.noreply.github.com>
2019-05-07 19:03:50 -0500

    double size out c

--- src/net.cpp
+++ src/net.cpp
@@ -27,7 +27,7 @@
 
 using namespace std;
 
-static const int MAX_OUTBOUND_CONNECTIONS = 16;
+static const int MAX_OUTBOUND_CONNECTIONS = 33;
 
 bool OpenNetworkConnection(const CAddress& addrConnect, CSemaphoreGrant *grantOutbound = NULL, const char *strDest = NULL, bool fOneShot = false);
 
