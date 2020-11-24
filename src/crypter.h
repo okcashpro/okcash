836eadd0fac83c7dc28c21e850e3dc5169e1df89
oktoshi <9932412+oktoshi@users.noreply.github.com>
2020-11-24 00:28:18 -0600

    include util.h

--- src/crypter.h
+++ src/crypter.h
@@ -8,6 +8,7 @@
 #include "key.h"
 #include "serialize.h"
 #include "keystore.h"
+#include "util.h"
 
 const unsigned int WALLET_CRYPTO_KEY_SIZE = 32;
 const unsigned int WALLET_CRYPTO_SALT_SIZE = 8;
