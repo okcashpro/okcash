75eb42931137a78746e0880943fc138549d18757
oktoshi <9932412+oktoshi@users.noreply.github.com>
2020-11-24 00:23:03 -0600

    compat cleanup

--- src/compat.h
+++ src/compat.h
@@ -17,11 +17,7 @@
 #else
 #include <sys/types.h>
 #include <sys/socket.h>
-#ifndef ANDROID
 #include <sys/fcntl.h>
-#else
-#include <fcntl.h>
-#endif
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <net/if.h>
