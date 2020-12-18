83c9961f9b947761520d76c493505a2a6cf7f3cd
oktoshi <9932412+oktoshi@users.noreply.github.com>
2020-12-18 08:48:35 -0600

    typo up

--- src/qt/coincontroldialog.h
+++ src/qt/coincontroldialog.h
@@ -14,7 +14,7 @@ namespace Ui {
     class CoinControlDialog;
 }
 class WalletModel;
-class OKCashBridge;
+class OkcashBridge;
 class CCoinControl;
 
 class CoinControlDialog : public QDialog
@@ -28,7 +28,7 @@ public:
     void setModel(WalletModel *model);
 
     // static because also called from sendcoinsdialog
-    static void updateLabels(WalletModel*, QDialog*, OKCashBridge *bridge = 0);
+    static void updateLabels(WalletModel*, QDialog*, OkcashBridge *bridge = 0);
     static QString getPriorityLabel(double);
 
     static QList<qint64> payAmounts;
