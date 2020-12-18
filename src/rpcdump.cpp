c4b3dea4c06fe8e1c58286e6a7b7202a92e6a476
oktoshi <9932412+oktoshi@users.noreply.github.com>
2020-12-18 06:53:00 -0600

    timestamp/ psize/ okchtaddrss

--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -5,8 +5,6 @@
 #include <iostream>
 #include <fstream>
 
-#include "util.h"
-#include "hash.h"
 #include "init.h" // for pwalletMain
 #include "rpcserver.h"
 #include "ui_interface.h"
@@ -115,9 +113,9 @@ public:
 
 Value importprivkey(const Array& params, bool fHelp)
 {
-    if (fHelp || params.size() < 1 || params.size() > 2)
+    if (fHelp || params.size() < 1 || params.size() > 4)
         throw runtime_error(
-            "importprivkey <okcashprivkey> [label]\n"
+            "importprivkey <okcashprivkey> [label] [rescan=true] [timestamp=1]\n"
             "Adds a private key (as returned by dumpprivkey) to your wallet.");
 
     string strSecret = params[0].get_str();
@@ -129,6 +127,10 @@ Value importprivkey(const Array& params, bool fHelp)
     bool fRescan = true;
     if (params.size() > 2)
         fRescan = params[2].get_bool();
+
+    int64_t nCreateTime = 1;
+    if (params.size() > 3)
+        nCreateTime = params[3].get_int64();
 
     CBitcoinSecret vchSecret;
     bool fGood = vchSecret.SetString(strSecret);
@@ -150,19 +152,21 @@ Value importprivkey(const Array& params, bool fHelp)
         if (pwalletMain->HaveKey(vchAddress))
             return Value::null;
 
-        pwalletMain->mapKeyMetadata[vchAddress].nCreateTime = 1;
+        pwalletMain->mapKeyMetadata[vchAddress].nCreateTime = nCreateTime;
 
         if (!pwalletMain->AddKeyPubKey(key, pubkey))
             throw JSONRPCError(RPC_WALLET_ERROR, "Error adding key to wallet");
 
         // whenever a key is imported, we need to scan the whole chain
-        pwalletMain->nTimeFirstKey = 1; // 0 would be considered 'no value'
+        pwalletMain->nTimeFirstKey = nCreateTime; // 0 would be considered 'no value'
 
         if (fRescan) {
             pwalletMain->ScanForWalletTransactions(pindexGenesisBlock, true);
             pwalletMain->ReacceptWalletTransactions();
         }
     }
+
+    SecureMsgAddWalletAddresses();
 
     return Value::null;
 }
@@ -255,23 +259,37 @@ Value dumpprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() != 1)
         throw runtime_error(
-            "dumpprivkey <okcashaddress>\n"
+            "dumpprivkey <okcashaddress/public_key_hex>\n"
             "Reveals the private key corresponding to <okcashaddress>.");
 
     EnsureWalletIsUnlocked();
 
-    string strAddress = params[0].get_str();
-    CBitcoinAddress address;
-    if (!address.SetString(strAddress))
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Okcash address");
     if (fWalletUnlockStakingOnly)
         throw JSONRPCError(RPC_WALLET_UNLOCK_NEEDED, "Wallet is unlocked for staking only.");
+
+    string strAddress = params[0].get_str();
+
+    CBitcoinAddress address;
     CKeyID keyID;
-    if (!address.GetKeyID(keyID))
-        throw JSONRPCError(RPC_TYPE_ERROR, "Address does not refer to a key");
+    bool fIsPubkey = false;
+    if (IsHex(strAddress) && strAddress.size() > 60) // pubkey input
+    {
+        CPubKey pk(ParseHex(strAddress));
+        if (!pk.IsValid())
+            throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid public key");
+        keyID = pk.GetID();
+        fIsPubkey = true;
+    } else
+    {
+        if (!address.SetString(strAddress))
+            throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Okcash address");
+        if (!address.GetKeyID(keyID))
+            throw JSONRPCError(RPC_TYPE_ERROR, "Address does not refer to a key");
+    };
+
     CKey vchSecret;
     if (!pwalletMain->GetKey(keyID, vchSecret))
-        throw JSONRPCError(RPC_WALLET_ERROR, "Private key for address " + strAddress + " is not known");
+        throw JSONRPCError(RPC_WALLET_ERROR, std::string("Private key for ")+(fIsPubkey ? "public key " : "address ") + strAddress + " is not known");
     return CBitcoinSecret(vchSecret).ToString();
 }
 
