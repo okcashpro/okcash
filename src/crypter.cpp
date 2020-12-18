335bdd31cf5def7a66f921171a0c9ad41f03f74f
oktoshi <9932412+oktoshi@users.noreply.github.com>
2020-12-18 05:44:24 -0600

    unlock

--- src/crypter.cpp
+++ src/crypter.cpp
@@ -238,8 +238,11 @@ bool CCryptoKeyStore::Unlock(const CKeyingMaterial& vMasterKeyIn)
         
         if (nUnlocked < 1) // at least 1 key must pass the test
         {
-            LogPrintf("Unlock failed: No keys unlocked.\n");
-            return false;
+            if (mapCryptedKeys.size() > 0)
+            {
+                LogPrintf("Unlock failed: No keys unlocked.\n");
+                return false;
+            };
         };
         
         vMasterKey = vMasterKeyIn;
