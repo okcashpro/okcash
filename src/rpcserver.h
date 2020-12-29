f010ac109a695ee8a50e5670f5334dd45fe1da43
oktoshi <9932412+oktoshi@users.noreply.github.com>
2020-12-29 17:08:13 -0600

    new rpc calls / getwalletinfo / getblockchaininfo
    new rpc calls for services integrations

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -178,6 +178,8 @@ extern json_spirit::Value walletlock(const json_spirit::Array& params, bool fHel
 extern json_spirit::Value encryptwallet(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value validateaddress(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getinfo(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value getwalletinfo(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value getblockchaininfo(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value reservebalance(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value checkwallet(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value repairwallet(const json_spirit::Array& params, bool fHelp);
