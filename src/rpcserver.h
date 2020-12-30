0a50423c1545ae8edf051cfe9ffdc1e97ad7afcc
oktoshi <9932412+oktoshi@users.noreply.github.com>
2020-12-30 09:21:44 -0600

    New RPCmethod getblockheader

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -203,6 +203,7 @@ extern json_spirit::Value settxfee(const json_spirit::Array& params, bool fHelp)
 extern json_spirit::Value getrawmempool(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getblockhash(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getblock(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value getblockheader(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getblockbynumber(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value setbestblockbyheight(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value rewindchain(const json_spirit::Array& params, bool fHelp);
