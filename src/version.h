d68116c5fc93889ecd07cc55ba051a46bfd661cc
oktoshi <devteam@okcash.co>
2022-02-04 14:21:59 -0600

    min proto version

--- src/version.h
+++ src/version.h
@@ -38,7 +38,7 @@ static const int PROTOCOL_VERSION = 70023;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60022;
+static const int MIN_PEER_PROTO_VERSION = 60023;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
