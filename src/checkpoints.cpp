1e536235b8535c28a37c2c292c4372591884c4f1
oktoshi <devteam@okcash.co>
2022-02-04 13:44:31 -0600

    sec chkp timestamp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -79,7 +79,7 @@ namespace Checkpoints
     ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1609328560, // * UNIX timestamp of last checkpoint block
+        1644001952, // * UNIX timestamp of last checkpoint block
         11011160,   // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         5000.0     // * estimated number of transactions per day after checkpoint
