7c5bb4ec8c93ee2f2b489b6c9595c19e7a759f02
oktoshi <devteam@okcash.co>
2022-11-19 06:03:04 -0600

    v OK

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both okcashd and okcash, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("oktoshi");
+const std::string CLIENT_NAME("OK");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   ""
