55a7b9d4982cf80de4b9ec53fa9693f1b50c06ec
oktoshi <devteam@okcash.co>
2023-02-06 01:11:55 -0600

    updated syncok.sh

--- oktools/ubuntu2004/syncok.sh
+++ oktools/ubuntu2004/syncok.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Initializing Okcash First time instant Sync"
-echo "This could take a minute, enjoy some coffee or water and come back soon..."
+echo "Initializing the 1 Click Instant Sync for the OK Blockchain"
+echo "This could take a minute, enjoy some coffee and come back soon..."
 
 date
 
@@ -8,21 +8,21 @@ date
 
 mkdir ~/.okcash
 
-sudo apt-get install unzip pwgen -y
+sudo apt-get install unzip -y
 
-#make sure old blockchain zip or not fully downloaded data gets erased to prevent errors
+#make sure old ok-blockchain.zip, or not fully downloaded data gets erased to prevent errors
 rm ~/.okcash/ok-blockchain.zip
 
 # Download and unzip the OK Blockchain
 cd ~/.okcash
-wget https://github.com/okcashpro/ok-blockchain/releases/download/latest/ok-blockchain.zip
+wget https://okcash.co/ok-blockchain.zip
 unzip ok-blockchain.zip
 cd
 
-# Delete the downloaded blockchain zip file // free space from device
+# Delete the downloaded ok-blockchain.zip file // free space from device
 #rm ~/.okcash/ok-blockchain.zip
 
 # end Client
-echo "Completed Okcash first time instant sync, make sure you have an okcash.conf file if you are goin to use okcashd daemon."
+echo "Completed the OK Blockchain 1 Click Instant sync, make sure you have an okcash.conf file if you are goin to use okcashd daemon."
 echo "enjoy your OK experience"
 exit 0
