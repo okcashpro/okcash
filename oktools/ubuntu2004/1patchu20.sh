94ed9d5943dc651399cc901d6bfbf511374e6789
oktoshi <devteam@okcash.co>
2023-11-15 13:45:07 -0600

    ubuntu 20 patch for compilation

--- oktools/ubuntu2004/1patchu20.sh
+++ oktools/ubuntu2004/1patchu20.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Patch for Okcash for Manjaro Linux 5205"
+echo "Patch for Okcash for Ubuntu 20"
 
 date
 
@@ -9,7 +9,7 @@ cp -r patch2004/* ../..
 echo "PATCH APPLIED"
 
 # end Client
-echo "Done with the Okcash Manjaro patch 2004 on its latest version for:"
+echo "Done with the Okcash Ubuntu 20 patch on its latest version for:"
 uname -a
 echo "Get empowered with Okcash!"
 exit 0
