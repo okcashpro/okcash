0730314c0b13002e6f6127ce8badd8ad12777d41
oktoshi <devteam@okcash.co>
2024-02-26 17:27:39 -0600

    ubuntu 22.04 qt-creator install upgrade / deps

--- oktools/ubuntu22/6depsgui.sh
+++ oktools/ubuntu22/6depsgui.sh
@@ -8,7 +8,7 @@ date
 
 cd
 
-sudo apt-get install qtcreator qttools5-dev-tools  libqt5webkit5-dev qt5-default -y
+sudo apt-get install qtcreator qttools5-dev-tools libqt5webkit5-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools -y
 
 echo "All the required dependencies for Okcash Graphical client are now installed for:"
 uname -a
