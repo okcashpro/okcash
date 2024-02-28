b7ee1cd8fd71c597d8a85b20a1eb2b9c781853a6
oktoshi <devteam@okcash.co>
2024-02-28 04:25:46 -0600

    1 Click Install Files for Ubuntu20.04

---
+++ oktools/ubuntu20/Compile_ubuntu20_GUI.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "Compiling okcash GUI (Qt) for the system"
+
+# Patch Okcash for Ubuntu 20
+/bin/bash ./1patchu20.sh
+
+#Install libssl1.0-dev
+/bin/bash ./2libssl10.sh
+
+#Install deps for okcash
+/bin/bash ./3depsnode.sh
+/bin/bash ./6depsgui.sh
+
+#Compile and Install db-4.8.30
+/bin/bash ./4db4830.sh
+
+#Setup db-4.8.30 variables
+export BDB_PREFIX="/usr/local/BerkeleyDB.4.8"
+export BDB_INCLUDE_PATH="/usr/local/BerkeleyDB.4.8/include"
+export BDB_LIB_PATH="/usr/local/BerkeleyDB.4.8/lib"
+export CPATH="/usr/local/BerkeleyDB.4.8/include"
+export LIBRARY_PATH="/usr/local/BerkeleyDB.4.8/lib"
+
+# Build okcash
+cd
+cd okcash
+qmake
+make 
+strip okcash
+
+# Install okcash GUI in the system
+sudo cp okcash /usr/local/bin
+echo "= okcash GUI (Qt) is now build and installed for ="
+uname -a
+
+#exit
+exit 0
