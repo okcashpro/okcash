ef4a3d5d29bb2a16ff94f63feecf4c8740214376
oktoshi <devteam@okcash.co>
2024-02-29 03:16:40 -0600

    ssl 1.1 + 3 compat install u20

---
+++ oktools/ubuntu20/2libssl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "installing libssl-dev to build Okcash node and graphical client."
+#Install libssl-dev
+sudo apt-get install libssl-dev -y
+
+echo "libssl is now installed"
+
+exit 0
