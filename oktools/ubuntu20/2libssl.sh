89ec7dfdd93dd11b683a71341b997d933f354751
oktoshi <devteam@okcash.co>
2024-03-01 00:15:59 -0600

    ubuntu all upgrade cleanup

--- oktools/ubuntu20/2libssl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "installing libssl-dev to build Okcash node and graphical client."
-#Install libssl-dev
-sudo apt-get install libssl-dev -y
-
-echo "libssl is now installed"
-
-exit 0
