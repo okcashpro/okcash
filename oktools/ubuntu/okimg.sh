8e1e4ea1fe84a58a04d57c30a6de8cc6c5bf64c4
oktoshi <devteam@okcash.co>
2024-03-05 19:24:10 -0600

    okimg

--- oktools/ubuntu/okimg.sh
+++ oktools/ubuntu/okimg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ../..
 mkdir -p appdir/usr/bin ; strip okcash ; cp okcash ./appdir/usr/bin/
-mkdir -p appdir/usr/share/applications ; cp oktools/ubuntu/snap/okcash.desktop ./appdir/usr/share/applications/
+mkdir -p appdir/usr/share/applications ; cp oktools/ubuntu/appimage/okcash.desktop ./appdir/usr/share/applications/
 mkdir -p appdir/usr/share/icons/hicolor/256x256/apps ; cp oktools/ubuntu/okcash-icon.png ./appdir/usr/share/icons/hicolor/256x256/apps/
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
