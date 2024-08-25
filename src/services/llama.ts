130456fdb33e95f58554870cbf9f2b772517fdbb
moon <shawmakesmagic@gmail.com>
2024-08-24 23:21:40 -0700

    remove const

--- src/services/llama.ts
+++ src/services/llama.ts
@@ -371,7 +371,7 @@ class LlamaService {
       if (!jsonString) {
         // try parsing response as JSON
         try {
-          const jsonString = JSON.stringify(JSON.parse(response));
+          jsonString = JSON.stringify(JSON.parse(response));
           console.log("parsedResponse", jsonString);
         } catch {
           throw new Error("JSON string not found");
