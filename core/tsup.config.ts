754941ca0d1ce1a059ab36add0a813896a256039
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-11-07 14:07:19 +1100

    fix

--- core/tsup.config.ts
+++ core/tsup.config.ts
@@ -5,11 +5,13 @@ export default defineConfig({
     outDir: "dist",
     sourcemap: true,
     clean: true,
-    format: ["cjs"], // Ensure you're targeting CommonJS
+    format: ["esm"], // Ensure you're targeting CommonJS
     external: [
         "dotenv", // Externalize dotenv to prevent bundling
         "fs", // Externalize fs to use Node.js built-in module
         "path", // Externalize other built-ins if necessary
+        "http",
+        "https"
         // Add other modules you want to externalize
     ],
 });
--- packages/plugin-image-generation/tsup.config.ts
+++ packages/plugin-image-generation/tsup.config.ts
@@ -5,13 +5,16 @@ export default defineConfig({
     outDir: "dist",
     sourcemap: true,
     clean: true,
-    format: ["cjs"], // Ensure you're targeting CommonJS
+    format: ["esm"], // Ensure you're targeting CommonJS
     external: [
         "dotenv", // Externalize dotenv to prevent bundling
         "fs", // Externalize fs to use Node.js built-in module
         "path", // Externalize other built-ins if necessary
         "@reflink/reflink",
         "@node-llama-cpp",
+        "https",
+        "http",
+        "agentkeepalive",
         // Add other modules you want to externalize
     ],
 });
