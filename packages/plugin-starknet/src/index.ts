d4394c26be3e94c7af7e4b8702e4f5cb873f00de
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-11-14 21:30:30 +1100

    plugin schema

--- packages/plugin-starknet/src/index.ts
+++ packages/plugin-starknet/src/index.ts
@@ -1,23 +1,16 @@
 import { Plugin } from "@ai16z/eliza";
-// import { executeSwap } from "./actions/swap.ts";
-// import take_order from "./actions/takeOrder";
-// import pumpfun from "./actions/pumpfun";
-// import { executeSwapForDAO } from "./actions/swapDao";
-// import { walletProvider } from "./providers/wallet.ts";
-// import { trustScoreProvider } from "./providers/trustScoreProvider.ts";
-// import { trustEvaluator } from "./evaluators/trust.ts";
+import { executeSwap } from "./actions/swap";
+import {
+    getStarknetAccountProvider,
+    getStarknetRpcProvider,
+} from "./providers/avnu";
 
 export const starknetPlugin: Plugin = {
     name: "Starknet",
-    description: "Starknet Plugin for Eliza",
-    actions: [
-        // executeSwap,
-        // pumpfun,
-        // executeSwapForDAO,
-        // take_order,
-    ],
+    description: "Starknet Swap Plugin for Eliza",
+    actions: [executeSwap],
     evaluators: [],
-    providers: [],
+    providers: [getStarknetAccountProvider, getStarknetRpcProvider],
 };
 
 export default starknetPlugin;
