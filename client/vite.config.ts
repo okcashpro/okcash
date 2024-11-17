4f3fd20e98b5224bcce90dc77de57c976605c850
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-11-18 08:59:38 +1100

    client

---
+++ client/vite.config.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
+});
