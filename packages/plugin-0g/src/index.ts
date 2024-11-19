360e66cf883fecc3037f7c2a77da28adeecb0684
root <root@vmi2228642.contaboserver.net>
2024-11-19 08:56:58 +0100

    add readme of plugin-0g

--- packages/plugin-0g/src/index.ts
+++ packages/plugin-0g/src/index.ts
@@ -2,8 +2,8 @@ import { Plugin } from "@ai16z/eliza";
 import { zgUpload } from "./actions/upload";
 
 export const zgPlugin: Plugin = {
-    name: "ZG",
     description: "0G Plugin for Eliza",
+    name: "ZeroG",
     actions: [zgUpload],
     evaluators: [],
     providers: [],
