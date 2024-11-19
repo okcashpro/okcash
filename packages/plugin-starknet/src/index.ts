8ff12696b6251d5a9be78bdf3b42a7b261dc457a
RedBeard <90423049+RedBeardEth@users.noreply.github.com>
2024-11-20 00:07:34 +1100

    init deploy unruggable memecoin
    Adds the deploy token action from Unruggable Memes

--- packages/plugin-starknet/src/index.ts
+++ packages/plugin-starknet/src/index.ts
@@ -1,7 +1,7 @@
 import { Plugin } from "@ai16z/eliza";
 import { executeSwap } from "./actions/swap";
 import transfer from "./actions/transfer";
-
+import { deployToken } from "./actions/unruggable";
 export const PROVIDER_CONFIG = {
     AVNU_API: "https://starknet.impulse.avnu.fi/v1",
     MAX_RETRIES: 3,
@@ -20,7 +20,7 @@ export const PROVIDER_CONFIG = {
 export const starknetPlugin: Plugin = {
     name: "starknet",
     description: "Starknet Plugin for Eliza",
-    actions: [transfer, executeSwap],
+    actions: [transfer, executeSwap, deployToken],
     evaluators: [],
     providers: [],
 };
