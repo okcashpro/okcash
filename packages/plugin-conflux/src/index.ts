f0975e586bf62235c46583198139ac0486a7a057
darwintree <17946284+darwintree@users.noreply.github.com>
2024-11-21 11:20:17 +0800

    feat(plugin-conflux): support bridge transfer

--- packages/plugin-conflux/src/index.ts
+++ packages/plugin-conflux/src/index.ts
@@ -1,9 +1,10 @@
 import { Plugin } from "@ai16z/eliza";
 import { transfer } from "./actions/transfer";
+import { bridgeTransfer } from "./actions/bridgeTransfer";
 
 export const confluxPlugin: Plugin = {
     name: "conflux",
     description: "Conflux Plugin for Eliza",
-    actions: [transfer],
+    actions: [transfer, bridgeTransfer],
     providers: [],
 };
