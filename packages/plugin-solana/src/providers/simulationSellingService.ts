5877e42fef044e50e15e1e68d9bc1a855dbf0e4f
MarcoMandar <malicemandar@gmail.com>
2024-11-25 23:50:23 +0200

    fix no-unused-vars
    Signed-off-by: MarcoMandar <malicemandar@gmail.com>

--- packages/plugin-solana/src/providers/simulationSellingService.ts
+++ packages/plugin-solana/src/providers/simulationSellingService.ts
@@ -9,7 +9,7 @@ import { Connection, PublicKey } from "@solana/web3.js";
 // Assuming TokenProvider and IAgentRuntime are available
 import { TokenProvider } from "./token.ts";
 // import { settings } from "@ai16z/eliza";
-import { IAgentRuntime, Memory, Provider, State } from "@ai16z/eliza";
+import { IAgentRuntime } from "@ai16z/eliza";
 import { WalletProvider } from "./wallet.ts";
 import * as amqp from "amqplib";
 
