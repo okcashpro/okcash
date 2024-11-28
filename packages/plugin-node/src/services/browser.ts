7f941495f324d4bdce9f6f65a68f6296810001d2
cygaar <tmpftl1@gmail.com>
2024-11-27 23:55:25 -0500

    Update useragent

--- packages/plugin-node/src/services/browser.ts
+++ packages/plugin-node/src/services/browser.ts
@@ -98,7 +98,7 @@ export class BrowserService extends Service implements IBrowserService {
 
             this.context = await this.browser.newContext({
                 userAgent:
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
             });
 
             this.blocker =
