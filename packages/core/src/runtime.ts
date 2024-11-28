7d746e6cafe27526edf29985aa802e5dd11a5cdc
yoniebans <jonny@techashai.com>
2024-11-28 10:10:22 +0000

    given the option for distinction between the model being used for generateText and generateImage, added a new property for the imageModelProvider on the runtime class. This property is set to the imageModelProvider on character if present, otherwise it defaults to the model provider hence, the same model will be used for bother generateText and generateImage

--- packages/core/src/runtime.ts
+++ packages/core/src/runtime.ts
@@ -99,6 +99,11 @@ export class AgentRuntime implements IAgentRuntime {
     modelProvider: ModelProviderName;
 
     /**
+     * The model to use for generateImage.
+     */
+    imageModelProvider: ModelProviderName;
+
+    /**
      * Fetch function to use
      * Some environments may not have access to the global fetch function and need a custom fetch override.
      */
@@ -303,7 +308,12 @@ export class AgentRuntime implements IAgentRuntime {
             opts.modelProvider ??
             this.modelProvider;
 
+        this.imageModelProvider =
+            this.character.imageModelProvider ??
+            this.modelProvider;
+
         elizaLogger.info("Selected model provider:", this.modelProvider);
+        elizaLogger.info("Selected image model provider:", this.imageModelProvider);
 
         // Validate model provider
         if (!Object.values(ModelProviderName).includes(this.modelProvider)) {
