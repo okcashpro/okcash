7fc7131948cdb3504f09a36213d9629f74db7a15
Tang Bo Hao <tech@btang.cn>
2024-12-03 17:09:15 +0800

    feat: init flow plugin

---
+++ packages/plugin-flow/src/index.ts
@@ -0,0 +1,11 @@
+import { Plugin } from "@ai16z/eliza";
+
+export const flowPlugin: Plugin = {
+    name: "flow",
+    description: "Flow Plugin for Eliza",
+    actions: [],
+    evaluators: [],
+    providers: [],
+};
+
+export default flowPlugin;
