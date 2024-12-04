8935f77c18f88febfdf44a58fd0caf2cbb11c179
Firat Sertgoz <f@nuff.tech>
2024-12-05 00:29:53 +0100

    feat: native transfers

--- packages/plugin-near/src/index.ts
+++ packages/plugin-near/src/index.ts
@@ -2,12 +2,13 @@ import { Plugin } from "@ai16z/eliza/src/types";
 import { walletProvider } from "./providers/wallet";
 // import { executeCreateToken } from "./actions/createToken";
 import { executeSwap } from "./actions/swap";
+import { executeTransfer } from './actions/transfer';
 
 export const nearPlugin: Plugin = {
     name: "NEAR",
     description: "Near Protocol Plugin for Eliza",
     providers: [walletProvider],
-    actions: [executeSwap],
+    actions: [executeSwap, executeTransfer],
     evaluators: [],
 };
 
