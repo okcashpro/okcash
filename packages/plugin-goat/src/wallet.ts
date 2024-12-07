aca549a7a4ec8cf88b27437daf3924fd35e1d83a
Agustin Armellini Fischer <armellini13@gmail.com>
2024-12-07 15:22:20 +0100

    Update chain to base

--- packages/plugin-goat/src/wallet.ts
+++ packages/plugin-goat/src/wallet.ts
@@ -2,11 +2,11 @@ import { WalletClient } from "@goat-sdk/core";
 import { viem } from "@goat-sdk/wallet-viem";
 import { createWalletClient, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-import { sepolia } from "viem/chains";
+import { base } from "viem/chains";
 
 // Add the chain you want to use, remember to update also
 // the EVM_PROVIDER_URL to the correct one for the chain
-export const chain = sepolia;
+export const chain = base;
 
 export function getWalletClient(
     getSetting: (key: string) => string | undefined
