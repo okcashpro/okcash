8b4b1f330c243dc211a0024304750cba2f60840c
oktoshi <devteam@okcash.co>
2024-12-10 06:40:55 -0600

    sync OKai to OK
    This integration ensures that all recent commits and contributors from OKai are reflected in OKcash, thereby properly giving recognition to the decentralized contributors we call OK Heroes. This synchronization supports the ongoing success and vibrancy of OK, aligning both repositories and fostering a unified development community.

---
+++ oktools/okaisync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+cd ..
+git remote add okai https://github.com/okcashpro/okai.git 2>/dev/null || echo "Remote 'okai' already exists."
+
+git fetch okai main
+
+git subtree pull --prefix=okai okai main
+
+git push origin master
+
+echo "Sync from okai to ok completed successfully."
