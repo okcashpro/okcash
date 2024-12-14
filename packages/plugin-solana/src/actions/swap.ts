7be712e46b89091a95954eae527fb1a152daf879
Shaw <shawmakesmagic@gmail.com>
2024-12-14 01:43:25 -0800

    remove extraneous deps

--- packages/plugin-solana/src/actions/swap.ts
+++ packages/plugin-solana/src/actions/swap.ts
@@ -1,24 +1,20 @@
-import { Connection, PublicKey, VersionedTransaction } from "@solana/web3.js";
-import BigNumber from "bignumber.js";
-import { v4 as uuidv4 } from "uuid";
-import { TrustScoreDatabase } from "@ai16z/plugin-trustdb";
 import {
     ActionExample,
+    composeContext,
+    generateObjectDeprecated,
     HandlerCallback,
     IAgentRuntime,
     Memory,
     ModelClass,
+    settings,
     State,
     type Action,
-    composeContext,
-    generateObjectDeprecated,
-    settings,
 } from "@ai16z/eliza";
-import { TokenProvider } from "../providers/token.ts";
-import { TrustScoreManager } from "../providers/trustScoreProvider.ts";
+import { Connection, PublicKey, VersionedTransaction } from "@solana/web3.js";
+import BigNumber from "bignumber.js";
+import { getWalletKey } from "../keypairUtils.ts";
 import { walletProvider, WalletProvider } from "../providers/wallet.ts";
 import { getTokenDecimals } from "./swapUtils.ts";
-import { getWalletKey } from "../keypairUtils.ts";
 
 async function swapToken(
     connection: Connection,
@@ -359,81 +355,6 @@ export const executeSwap: Action = {
             if (confirmation.value.err) {
                 throw new Error(
                     `Transaction failed: ${confirmation.value.err}`
-                );
-            }
-
-            if (type === "buy") {
-                const tokenProvider = new TokenProvider(
-                    response.outputTokenCA,
-                    provider,
-                    runtime.cacheManager
-                );
-                const module = await import("better-sqlite3");
-                const Database = module.default;
-                const trustScoreDb = new TrustScoreDatabase(
-                    new Database(":memory:")
-                );
-                // add or get recommender
-                const uuid = uuidv4();
-                const recommender = await trustScoreDb.getOrCreateRecommender({
-                    id: uuid,
-                    address: walletPublicKey.toString(),
-                    solanaPubkey: walletPublicKey.toString(),
-                });
-
-                const trustScoreDatabase = new TrustScoreManager(
-                    runtime,
-                    tokenProvider,
-                    trustScoreDb
-                );
-                // save the trade
-                const tradeData = {
-                    buy_amount: response.amount,
-                    is_simulation: false,
-                };
-                await trustScoreDatabase.createTradePerformance(
-                    runtime,
-                    response.outputTokenCA,
-                    recommender.id,
-                    tradeData
-                );
-            } else if (type === "sell") {
-                const tokenProvider = new TokenProvider(
-                    response.inputTokenCA,
-                    provider,
-                    runtime.cacheManager
-                );
-                const module = await import("better-sqlite3");
-                const Database = module.default;
-                const trustScoreDb = new TrustScoreDatabase(
-                    new Database(":memory:")
-                );
-                // add or get recommender
-                const uuid = uuidv4();
-                const recommender = await trustScoreDb.getOrCreateRecommender({
-                    id: uuid,
-                    address: walletPublicKey.toString(),
-                    solanaPubkey: walletPublicKey.toString(),
-                });
-
-                const trustScoreDatabase = new TrustScoreManager(
-                    runtime,
-                    tokenProvider,
-                    trustScoreDb
-                );
-                // save the trade
-                const sellDetails = {
-                    sell_amount: response.amount,
-                    sell_recommender_id: recommender.id,
-                };
-                const sellTimeStamp = new Date().getTime().toString();
-                await trustScoreDatabase.updateSellDetails(
-                    runtime,
-                    response.inputTokenCA,
-                    recommender.id,
-                    sellTimeStamp,
-                    sellDetails,
-                    false
                 );
             }
 
