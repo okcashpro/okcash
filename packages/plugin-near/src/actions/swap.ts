9052409c13fb371f69d59490ab16a88144472652
Shakker Nerd <shakkerdroid@gmail.com>
2024-12-14 10:53:09 +0000

    fix: remove unused variables in swap action

--- packages/plugin-near/src/actions/swap.ts
+++ packages/plugin-near/src/actions/swap.ts
@@ -57,7 +57,8 @@ async function swapToken(
             runtime.getSetting("RPC_URL") || "https://rpc.testnet.near.org";
 
         // Get all pools for estimation
-        const { ratedPools, unRatedPools, simplePools } = await fetchAllPools();
+        // ratedPools, unRatedPools,
+        const { simplePools } = await fetchAllPools();
         const swapTodos = await estimateSwap({
             tokenIn,
             tokenOut,
